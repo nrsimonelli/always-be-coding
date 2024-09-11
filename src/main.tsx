8c789a9ae6c254f450375f3b6d1845e84d71bc6b
nrsimonelli <nrsimonelli@gmail.com>
2024-09-10 19:25:12 -0500

    moved styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Home } from './components/home.tsx'
-import { Toaster } from './components/ui/toaster.tsx'
-import './index.css'
+import { Home } from '@/components/home.tsx'
+import { Toaster } from '@/components/ui/toaster.tsx'
+import '@/styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
