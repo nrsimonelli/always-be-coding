8f801e9115feddb81a2ba846a736966aff1e5c6c
nrsimonelli <nrsimonelli@gmail.com>
2024-09-14 19:37:44 -0500

    no more toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Home } from '@/components/home.tsx'
-import { Toaster } from '@/components/ui/toaster.tsx'
 import '@/styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Home />
-    <Toaster />
   </StrictMode>
 )
